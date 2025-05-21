0e675816de7185883702f86e3d199845c05e3f30
ArickCoder <arickff02@gmail.com>
2025-05-21 22:40:41 +0600

    Add files via upload

--- index.js
+++ index.js
@@ -10,7 +10,20 @@ fs.unlink('newTextfile.txt', function (err) {
     console.log('File Deleted!')
 })
 
-fs.rename('file.txt', 'renamedfile.txt', function (err){
+fs.rename('demofile.txt', 'renamedfile2.txt', function (err){
     if (err) throw err;
     console.log('File renamed as renamedfile.txt');
 })
+
+
+
+
+fs.readFile('renamedfile.txt', function (err){
+    if (err) throw err;
+    console.log('File Open');
+});
+
+fs.appendFile('renamedfile.txt', 'Hello User!', function (err){
+    if (err) throw err;
+    console.log('append');
+});
